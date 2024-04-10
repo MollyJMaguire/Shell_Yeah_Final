8909206ee4ad56f18921ce346797959679a3f04a
MollyJMaguire <165952253+MollyJMaguire@users.noreply.github.com>
2024-04-10 15:14:55 -0500

    Update state_change_timer.ino

--- state_change_timer.ino
+++ state_change_timer.ino
@@ -1,23 +1,33 @@
 #include <Servo.h>
-//SC1
+//SC1 :)
   const int switchPin1 = 8;
   int switchVal1 = 0;
   int prevSwitchVal1 = 0;
   Servo myservo1;
-//SC2
+//SC2 :o
   const int switchPin2 = 10;
   int switchVal2 = 0;
   int prevSwitchVal2 = 0;
   Servo myservo2;
-//SC3
+//SC3 >:(
   const int switchPin3 = 12;
   int switchVal3 = 0;
   int prevSwitchVal3 = 0;
   Servo myservo3;
+//SCT4 ;)
+  const int switchPin4 = 6;
+  int switchVal4 = 0;
+  int prevSwitchVal4 = 0;
+  Servo myservo4;
+  unsigned long timerVal;
+  unsigned long currentTime;
+  long timerLength = 3000;
+  bool timer = false;
 
 //State Change 1(Sword Breaks 180)
 //State Change 2 (Portrait 180)
 //State Change 3 (Lady Lake 90)
+//State Change w/timer 4 (sword in stone 180)
 void setup() {
 //SC1
   pinMode(switchPin1, INPUT);
@@ -31,6 +41,10 @@ void setup() {
   pinMode(switchPin3, INPUT);
   myservo3.attach(10);
   myservo3.write(0);
+//SCT4
+  pinMode(switchPin4, INPUT);
+  myservo4.attach(10);
+  myservo4.write(0);
 }
 
 void loop() {
@@ -56,45 +70,23 @@ switchVal2 = digitalRead(switchPin2);
 switchVal3 = digitalRead(switchPin3);
   if (switchVal3 != prevSwitchVal3){
     if (switchVal3 == HIGH){
-      myservo3.write(180);
+      myservo3.write(90);
     }
   }
-  prevSwitchVal2 = switchVal2;
-}
-//State Change w/timer (sword in stone)
-const int switchPin = 8;
-unsigned long timerVal;
-unsigned long currentTime;
-long timerLength = 3000;
-
-bool timer = false;
-int switchVal = 0;
-int prevSwitchVal = 0;
-
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(switchPin, INPUT);
-  myservo.attach(10);
-  myservo.write(0);
-
-}
-
-void loop() {
+  prevSwitchVal3 = switchVal3;
+//SCT4
   currentTime = millis();
-  switchVal = digitalRead(switchPin);
-  
+  switchVal4 = digitalRead(switchPin4);
   if (switchVal != prevSwitchVal){
     if (switchVal == HIGH){
       timerVal = currentTime;
       timer = true;
     }
   }
-
   if (timer == true){
     if (currentTime - timerVal >= timerLength){
       myservo.write(180);
     }
   }
-
   prevSwitchVal = switchVal;
 }
